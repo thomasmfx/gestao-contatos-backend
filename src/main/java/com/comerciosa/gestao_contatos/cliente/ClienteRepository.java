f8ab84ee355ce09d46fe1322563c0fcd71e108ab
thomasmfx <thomasmoisesf@gmail.com>
2025-03-28 10:31:17 -0300

    🌟 feat: criar ClienteRepository interface

---
+++ src/main/java/com/comerciosa/gestao_contatos/cliente/ClienteRepository.java
@@ -0,0 +1,5 @@
+package com.comerciosa.gestao_contatos.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> { }
