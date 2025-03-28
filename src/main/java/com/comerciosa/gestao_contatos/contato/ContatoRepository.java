1306fc741905e444f0e9f19cebfee83d960010bc
thomasmfx <thomasmoisesf@gmail.com>
2025-03-28 11:55:31 -0300

    🌟 feat: criar ContatoRepository

---
+++ src/main/java/com/comerciosa/gestao_contatos/contato/ContatoRepository.java
@@ -0,0 +1,5 @@
+package com.comerciosa.gestao_contatos.contato;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> { }
