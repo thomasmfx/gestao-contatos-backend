757d7c35ce495d4f0daf54ce8583b341b8e031b6
thomasmfx <thomasmoisesf@gmail.com>
2025-03-28 11:55:00 -0300

    🌟 feat: criar ContatoController

---
+++ src/main/java/com/comerciosa/gestao_contatos/controller/ContatoController.java
@@ -0,0 +1,47 @@
+package com.comerciosa.gestao_contatos.controller;
+
+import com.comerciosa.gestao_contatos.cliente.Cliente;
+import com.comerciosa.gestao_contatos.cliente.ClienteRepository;
+import com.comerciosa.gestao_contatos.contato.Contato;
+import com.comerciosa.gestao_contatos.contato.ContatoRepository;
+import com.comerciosa.gestao_contatos.contato.ContatoRequestDTO;
+import com.comerciosa.gestao_contatos.contato.ContatoResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("contatos")
+public class ContatoController {
+
+    private final ContatoRepository repository;
+    private final ClienteRepository clienteRepository;
+
+    public ContatoController(ContatoRepository repository, ClienteRepository clienteRepository) {
+        this.repository = repository;
+        this.clienteRepository = clienteRepository;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<ContatoResponseDTO> saveContato(@RequestBody ContatoRequestDTO dados) {
+        Cliente cliente = clienteRepository.findById(dados.clienteId())
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+
+        Contato novoContato = new Contato(dados, cliente);
+        Contato contatoSalvo = repository.save(novoContato);
+
+        return ResponseEntity.ok(new ContatoResponseDTO(contatoSalvo));
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<ContatoResponseDTO> getAll() {
+
+        List<ContatoResponseDTO> listaContatos;
+        listaContatos = repository.findAll().stream().map(ContatoResponseDTO::new).toList();
+        return listaContatos;
+    }
+
+}
