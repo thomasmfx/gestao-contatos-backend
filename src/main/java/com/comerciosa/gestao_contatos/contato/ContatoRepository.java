43a86e564f2a7d18e0cb2925f4b3f1fa1a777d75
thomasmfx <thomasmoisesf@gmail.com>
2025-03-28 15:24:55 -0300

    🦄 refactor: mudar nome de método

--- src/main/java/com/comerciosa/gestao_contatos/contato/ContatoRepository.java
+++ src/main/java/com/comerciosa/gestao_contatos/contato/ContatoRepository.java
@@ -2,4 +2,8 @@ package com.comerciosa.gestao_contatos.contato;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContatoRepository extends JpaRepository<Contato, Integer> { }
+import java.util.List;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+    List<Contato> findByClienteId(Integer clienteId);
+}
