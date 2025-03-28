ec9116a7d361a65331f1eaf13f1626197a869a1a
thomasmfx <thomasmoisesf@gmail.com>
2025-03-27 22:51:08 -0300

    🎉 commit inicial

---
+++ src/main/java/com/comerciosa/gestao_contatos/GestaoContatosApplication.java
@@ -0,0 +1,13 @@
+package com.comerciosa.gestao_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comerciosa/gestao_contatos/GestaoContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comerciosa.gestao_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
