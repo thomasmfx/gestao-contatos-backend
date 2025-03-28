6af9ab0d2d640b2f3c0732d636f20d0920ef56bf
thomasmfx <thomasmoisesf@gmail.com>
2025-03-28 13:36:48 -0300

    🌟 feat: adicionar validação para cpf e nome do cliente
    - Ambos os campos não podem estar vazios

--- src/main/java/com/comerciosa/gestao_contatos/cliente/ClienteRequestDTO.java
+++ src/main/java/com/comerciosa/gestao_contatos/cliente/ClienteRequestDTO.java
@@ -1,11 +1,14 @@
 package com.comerciosa.gestao_contatos.cliente;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDate;
 
 public record ClienteRequestDTO(
+        @NotBlank(message = "O nome é obrigatório")
         String nome,
+        @NotBlank(message = "O CPF é obrigatório")
         String cpf,
         @Column(name = "data_nascimento")
         LocalDate dataNascimento,
