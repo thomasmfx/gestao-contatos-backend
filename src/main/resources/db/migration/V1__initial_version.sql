a0279a6c3df08dc8c4880a40823c26bd0365b4c7
Thomas <thomasmoisesf@gmail.com.br>
2025-06-13 14:07:04 -0300

    feat: implementa migrations com flyway

--- src/main/resources/db/migration/V1__initial_version.sql
+++ src/main/resources/db/migration/V1__initial_version.sql
@@ -1,3 +1,25 @@
+-- Schema
+CREATE TABLE cliente (
+   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   nome VARCHAR(100) NOT NULL,
+   cpf VARCHAR(14) UNIQUE NOT NULL,
+   data_nascimento DATE NOT NULL,
+   endereco VARCHAR(255)
+);
+
+CREATE TABLE contato (
+   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   cliente_id INTEGER NOT NULL,
+   tipo VARCHAR(50) NOT NULL,
+   valor VARCHAR(100) NOT NULL,
+   observacao VARCHAR(255),
+   CONSTRAINT fk_cliente
+       FOREIGN KEY (cliente_id)
+           REFERENCES cliente(id) ON DELETE CASCADE
+);
+
+-- Data
+
 -- Clientes
 
 INSERT INTO cliente (nome, cpf, data_nascimento, endereco)
---
+++ src/main/resources/db/migration/V2__add_rg_to_users.sql
@@ -0,0 +1,1 @@
+ALTER TABLE cliente ADD COLUMN rg VARCHAR(20);
